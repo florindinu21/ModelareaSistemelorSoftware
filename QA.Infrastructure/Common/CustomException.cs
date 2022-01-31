0e8d22e61fe4902566a12a218ae474279fc8e495
Rox <roxana.vasile@softbinator.com>
2022-01-31 15:37:49 +0200

    added common used cs classes

---
+++ QA.Infrastructure/Common/CustomException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace QA.Infrastructure.Common
+{
+    public class CustomException : Exception
+    {
+        public CustomException(string message)
+            : base(message) { }
+    }
+}
