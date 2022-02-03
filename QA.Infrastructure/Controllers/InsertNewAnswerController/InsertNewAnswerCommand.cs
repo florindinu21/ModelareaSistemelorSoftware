6b2304b589f2e1950f275ae0914fd58bf01e13a6
radubobe <radu-traian.bobe@my.fmi.unibuc.ro>
2022-02-03 21:59:11 +0200

    Added NewAnswerController and NewQuestion Controller

---
+++ QA.Infrastructure/Controllers/InsertNewAnswerController/InsertNewAnswerCommand.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+
+namespace QA.Infrastructure.Controllers.InsertNewAnswerController
+{
+    public class InsertNewAnswerCommand
+    {
+        public string ProtectedQuestionId { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class InsertNewAnswerCommandValidator
+        : AbstractValidator<InsertNewAnswerCommand>
+    {
+        public InsertNewAnswerCommandValidator()
+        {
+            RuleFor(x => x.ProtectedQuestionId)
+                .NotEmpty();
+
+            RuleFor(x => x.Description)
+                .NotEmpty()
+                    .WithMessage("You must provide a description.")
+                .MaximumLength(1600);
+        }
+    }
+}
