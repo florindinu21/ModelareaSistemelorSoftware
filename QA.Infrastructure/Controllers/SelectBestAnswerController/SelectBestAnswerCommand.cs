f7f6a443547d6adf495f8cd3b643ea4e13acdf01
florindinu21 <florin.dinu@my.fmi.unibuc.ro>
2022-02-04 13:06:51 +0200

    Insert Answer and Question Controllers

---
+++ QA.Infrastructure/Controllers/SelectBestAnswerController/SelectBestAnswerCommand.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace QA.Infrastructure.Controllers.SelectBestAnswerController
+{
+    public class SelectBestAnswerCommand
+    {
+        public string ProtectedAnswerId { get; set; }
+    }
+
+    public class SelectBestAnswerCommandValidator
+        : AbstractValidator<SelectBestAnswerCommand>
+    {
+        public SelectBestAnswerCommandValidator()
+        {
+            RuleFor(x => x.ProtectedAnswerId)
+                .NotEmpty();
+        }
+    }
+}
---
+++ QA.Infrastructure/Controllers/VoteForAnswerHandler/VoteForAnswerCommand.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace QA.Infrastructure.Controllers.VoteForAnswerHandler
+{
+    public class VoteForAnswerCommand
+    {
+        public string ProtectedAnswerId { get; set; }
+        public bool UpVote { get; set; }
+    }
+
+    public class VoteForAnswerCommandValidator
+        : AbstractValidator<VoteForAnswerCommand>
+    {
+        public VoteForAnswerCommandValidator()
+        {
+            RuleFor(x => x.ProtectedAnswerId)
+                .NotEmpty();
+        }
+    }
+}
